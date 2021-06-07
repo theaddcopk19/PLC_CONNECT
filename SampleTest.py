c8e7e584506dcfb038d2f753366d498bf0ff9369
theaddcopk19 <kurokawa.taiki.nia@gmail.com>
2021-06-07 17:07:48 +0700

    time out update

--- SampleTest.py
+++ SampleTest.py
@@ -8,5 +8,6 @@ def Divice():
         minute = data.Data_D(8014)
         second = data.Data_D(8013)
         sleep(1)
-        print("D8015:{0}, D8014:{1}, D8013:{2}".format(Hour, minute, second))
-Divice()
+        print("D8015:{0}, D8014:{1}, D8013:{2}".format(Hour,\
+             minute, second))
+
